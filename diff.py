7c7958e1f0bd1176dc54d020b93c963ff7e92063
kingwiiwii <kingwiiwii@users.noreply.github.com>
2016-06-09 16:57:08 +0100

    Added IF statements to check if file is in PWD
    some other small fixes/changes.

--- diff.py
+++ diff.py
@@ -9,14 +9,32 @@ print
 print "++++++++++++++++++++"
 print
 
-File1 = raw_input('File 1 : ')
-File2 = raw_input('File 2 : ')
+file1_in_PWD = raw_input('Is file 1 in the current directory [Y/N] : ')
+
+if file1_in_PWD == 'Y' or 'y' or 'Yes' or 'yes':
+	File1 = raw_input('File 1 : ')
+	file1_path = os.path.join(os.getcwd(), File1)
+elif file1_in_PWD == 'N' or 'n' or 'No' or 'no':
+	File1 = raw_input('Enter full path to file 1: ')
+
+
+file2_in_PWD = raw_input('Is file 2 in the current directory [Y/N] : ')
+
+if file2_in_PWD == 'Y' or 'y' or 'Yes' or 'yes':
+	File2 = raw_input('File 2 : ')
+	file2_path = os.path.join(os.getcwd(), File2)
+elif file2_in_PWD == 'N' or 'n' or 'No' or 'no':
+	File2 = raw_input('Enter full path to file 2: ')
+
+
 time.sleep(1.0)
+
+
 print
 print
 
-file1_path = os.path.join(os.getcwd(), File1)
-file2_path = os.path.join(os.getcwd(), File2)
+#file1_path = os.path.join(os.getcwd(), File1)
+#file2_path = os.path.join(os.getcwd(), File2)
 
 print "File 1 Path is : " , file1_path
 print "File 2 Path is : " , file2_path
@@ -35,20 +53,15 @@ open_file_time_stop = time.time()
 print
 print
 print "File Load Time - ", open_file_time_stop - open_file_time_start
-
 print
 print
-print "=================================================="
-print
-print "Comparing Files - ",File1, "and",File2
-print
-print "=================================================="
+print "Comparing Files -",File1, "and",File2
 print
 time.sleep(1.0)
 
 
 print
-print
+
 
 diff_file_time_start = time.time()
 
@@ -63,13 +76,13 @@ while f1_line != '' or f2_line != '':
 		diffs_found = True
 		count += 1
 		if f2_line == '' and f1_line != '':
-			print">+", "Line-%d" % line_no, f1_line
+			print">>>+", "Line-%d" % line_no, f1_line
 		elif f1_line != '':
-			print">", "Line-%d" % line_no, f1_line
+			print">>>", "Line-%d" % line_no, f1_line
 		if f1_line == '' and f2_line != '':
-			print"<+", "Line-%d" % line_no, f2_line
+			print"<<<+", "Line-%d" % line_no, f2_line
 		elif f2_line != '':
-			print"<", "Line-%d" %  line_no, f2_line
+			print"<<<", "Line-%d" %  line_no, f2_line
 		print""
 	
 	f1_line = file1_open.readline()
@@ -82,9 +95,12 @@ file2_open.close
 
 diff_file_time_stop = time.time()	
 
-print "File Diff Time - ", diff_file_time_stop - diff_file_time_start
+
 print
 if diffs_found == False:
 	print "Complete, files identical."
 elif diffs_found == True:
-	print "Complete, ",count, "differences found."
+	print "Complete,",count, "differences found."
+print
+print 
+print "File Diff Time - ", diff_file_time_stop - diff_file_time_start
