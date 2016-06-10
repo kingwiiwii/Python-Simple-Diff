8361729ce8ffc5448b5dc7ca599b2635a9fd6711
kingwiiwii <kingwiiwii@users.noreply.github.com>
2016-06-10 10:53:43 +0100

    Code before attempting file write
    large files have issue 
    
    >>>+ Line-4508 Inactive timeout reached, logging out.

--- diff.py
+++ diff.py
@@ -2,11 +2,11 @@ import os
 import time
 
 print
-print "++++++++++++++++++++"
+print "+++++++++++++++++++"
 print
 print " Simple Diff v0.1"
 print
-print "++++++++++++++++++++"
+print "+++++++++++++++++++"
 print
 
 file1_in_PWD = raw_input('Is file 1 in the current directory [Y/N] : ')
@@ -26,16 +26,18 @@ if file2_in_PWD == 'Y' or 'y' or 'Yes' or 'yes':
 elif file2_in_PWD == 'N' or 'n' or 'No' or 'no':
 	File2 = raw_input('Enter full path to file 2: ')
 
+write_to_file = raw_input('Write output to file ? [Y/N]' )
 
+if write_to_file == 'Y' or 'y' or 'Yes' or 'yes':
+	write_to_file = True
+elif write_to_file == 'N' or 'n' or 'No' or 'no':
+	write_to_file = False
+	
 time.sleep(1.0)
 
 
 print
 print
-
-#file1_path = os.path.join(os.getcwd(), File1)
-#file2_path = os.path.join(os.getcwd(), File2)
-
 print "File 1 Path is : " , file1_path
 print "File 2 Path is : " , file2_path
 time.sleep(1.0)
@@ -50,9 +52,6 @@ open_file_time_start = time.time()
 f1_line = file1_open.readline()
 f2_line = file2_open.readline()
 open_file_time_stop = time.time()
-print
-print
-print "File Load Time - ", open_file_time_stop - open_file_time_start
 print
 print
 print "Comparing Files -",File1, "and",File2
@@ -102,5 +101,9 @@ if diffs_found == False:
 elif diffs_found == True:
 	print "Complete,",count, "differences found."
 print
-print 
-print "File Diff Time - ", diff_file_time_stop - diff_file_time_start
+print
+load_time = open_file_time_stop - open_file_time_start
+diff_time = diff_file_time_stop - diff_file_time_start
+print "File Load Time - ", load_time 
+print "File Diff Time - ", diff_time
+print "Run Time - ", load_time + diff_time
